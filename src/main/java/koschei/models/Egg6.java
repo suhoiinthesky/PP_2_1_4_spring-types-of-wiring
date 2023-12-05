08901915a76342134cd2c0ab8d1c4e42c1910bad
suhoiinthesky <maicol-gordan@mail.ru>
2023-12-05 20:18:22 +0300

    "Task done"

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,15 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Egg6 {
+    @Autowired
+    private Deth8 depth;
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + depth.toString();
     }
 }
